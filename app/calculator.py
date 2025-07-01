766629a9b8e6814012ed8c7b1a794a67f4300e84
Siraj Shaik <siraj.shaik@juspay.in>
2025-07-01 13:59:26 +0530

    hello world this branch should be merged

--- app/calculator.py
+++ app/calculator.py
@@ -3,19 +3,20 @@
 # this is test for wrong commit push hopely
 # this should let be merged
 # hello made a new branhc named  fix-calculator one 
+# this branch shpould be merged to main branch
 
 def add(a, b):
-    return a + b+1 
+    return a + b
 
 def subtract(a, b):
-    return a - b+1 
+    return a - b
 
 def multiply(a, b):
-    return a * b+1 
+    return a * b
 
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
-    return a / b+1
+    return a / b
 
 
